eee9b5fbdecfed94a1f34eaff2e2191734f70ab5
Mattia Peri <mattia.peri@ynap.it>
2019-07-04 18:54:50 +0200

    Added metrics-server, weave-scope, prometheus-operator and istio

--- k3s-multipass-cluster.bash
+++ k3s-multipass-cluster.bash
@@ -5,31 +5,38 @@ dictionary() {
 # master must be just 1 and must be the first 
 # nodename cpus memory disk
 cat << EOF
-k3s-master 1 1024M 3G
-k3s-worker1 1 1024M 3G
-k3s-worker2 1 1024M 3G
+k3s-master 1 1G 512M
+k3s-worker1 3 3G 5G
+k3s-worker2 3 3G 5G
+k3s-worker3 3 3G 5G
 EOF
 }
+# with 3G no space left during istio installation
 
 k3s_master_cloud_init() {
 cat << EOF
+#cloud-config
 runcmd:
- - '\curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - --node-taint node-role.kubernetes.io/master=effect:NoSchedule'
+ - '\curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - --node-taint node-role.kubernetes.io/master=effect:NoSchedule --no-deploy=traefik'
 EOF
 }
 
 k3s_worker_cloud_init() {
 cat << EOF
+#cloud-config
 runcmd:
- - '\curl -sfL https://get.k3s.io | K3S_TOKEN=${K3S_TOKEN} K3S_URL=${K3S_NODEIP_MASTER} sh -s -'
+ - '\curl -sfL https://get.k3s.io | K3S_TOKEN=${K3S_TOKEN} K3S_URL=${K3S_URL} sh -s -'
 EOF
 }
 
 KUBECONFIG_PATH="${HOME}/.kube/k3s.yaml"
 K3S_NODEIP_MASTER=""
+K3S_URL=""
 LEADING_NODE=""
+K3S_NODEIP_WORKER=""
 KUBECTL_INSTALLED=""
 HELM_INSTALLED=""
+INSTALL_K3S_EXEC_WORKER=""#"--no-deploy=traefik --no-deploy=servicelb"
 
 ### Functions
 
@@ -66,6 +73,15 @@ fatal()
 }
 
 # --- prerequisite functions ---
+
+check_root() {
+    if [[ $USER == root || $HOME == /root ]] ; then
+        fatal "Please don't run as root"
+        info "Sudo will be used internally by this script as required."
+        exit 1
+    fi
+}
+
 check_prerequisite()
 {
   ### Check osx running
@@ -73,9 +89,9 @@ check_prerequisite()
   ### Check brew installed
   [ ! -f "`command -v brew`" ] && fatal "brew not installed, visit https://brew.sh"
   ### Check kubectl installed
-  [ ! -f "`command -v kubectl`" ] && info "kubectl not installed, to install it: brew install kubernetes-cli" && KUBECTL_INSTALLED="true" || KUBECTL_INSTALLED="false"
+  [ ! -f "`command -v kubectl`" ] && info "kubectl not installed, to install it: brew install kubernetes-cli" && KUBECTL_INSTALLED="false" || KUBECTL_INSTALLED="true"
   ### Check helm installed
-  [ ! -f "`command -v helm`" ] && info "helm not installed, to install it: brew install kubernetes-helm" && HELM_INSTALLED="true" || HELM_INSTALLED="false"
+  [ ! -f "`command -v helm`" ] && info "helm not installed, to install it: brew install kubernetes-helm" && HELM_INSTALLED="false" || HELM_INSTALLED="true"
   success "Prerequisites: OK"
 }
 
@@ -117,7 +133,7 @@ k3s_node()
   [ $? -eq 0 ] && success "Node ${K3S_NAME} k3s creation: OK" || fatal "Node ${K3S_NAME} creation: KO"
   info "Node ${K3S_NAME} k3s: Waiting to be ready"
   # kubectl is configured only on master node
-  # we need double axes to pass variables to the string
+  # we need double apex to pass variables to the string
   # we need backslash on kubectl otherwise executed on host machine
   multipass exec ${LEADING_NODE} -- /bin/bash -c "while [[ \$(kubectl get nodes ${K3S_NAME} --no-headers 2>/dev/null | grep -c -w \"Ready\") -ne 1 ]]; do echo -n .; sleep 5; done; echo" < /dev/null
   [ $? -eq 0 ] && success "Node ${K3S_NAME} k3s: Ready" || fatal "Node ${K3S_NAME} k3s: KO"
@@ -130,11 +146,13 @@ k3s_setup()
     if [[ ${LINE[0]} == *"master"* ]]; then
       k3s_master_node ${LINE[@]}
       LEADING_NODE=${LINE[0]} && info "LEADING_NODE=${LEADING_NODE}"
-      K3S_NODEIP_MASTER="https://$(multipass info ${LEADING_NODE} | grep "IPv4" | awk -F' ' '{print $2}'):6443" && info "K3S_NODEIP_MASTER=${K3S_NODEIP_MASTER}"
+      K3S_NODEIP_MASTER=$(multipass info ${LEADING_NODE} | grep "IPv4" | awk -F' ' '{print $2}') && info "K3S_NODEIP_MASTER=${K3S_NODEIP_MASTER}"
+      K3S_URL="https://${K3S_NODEIP_MASTER}:6443" && info "K3S_URL=${K3S_URL}"
       K3S_TOKEN="$(multipass exec ${LEADING_NODE} -- /bin/bash -c "sudo cat /var/lib/rancher/k3s/server/node-token" < /dev/null)" && info "K3S_TOKEN=${K3S_TOKEN}"
     fi
     if [[ ${LINE[0]} == *"worker"* ]]; then
       k3s_node ${LINE[@]}
+      K3S_NODEIP_WORKER=$(multipass info ${LINE[0]} | grep "IPv4" | awk -F' ' '{print $2}') && info "K3S_NODEIP_WORKER=${K3S_NODEIP_WORKER}"
     fi
   done < <(dictionary)
 }
@@ -157,10 +175,10 @@ k3s_labels() #OBSOLETE DUE TO: https://github.com/rancher/k3s/issues/379
 
 kubectl_configuration()
 {
-  if [ ${KUBECTL_INSTALLED} ]; then
+  if ${KUBECTL_INSTALLED}; then
       if [ -w ${KUBECONFIG_PATH} ]; then
           multipass copy-files ${LEADING_NODE}:/etc/rancher/k3s/k3s.yaml ${KUBECONFIG_PATH}
-          sed -ie s,https://localhost:6443,${K3S_NODEIP_MASTER},g ${KUBECONFIG_PATH}
+          sed -ie s,https://localhost:6443,${K3S_URL},g ${KUBECONFIG_PATH}
           kubectl --kubeconfig=${KUBECONFIG_PATH} get nodes
           [ $? -eq 0 ] && success "kubectl configuration: OK" && info "Use i.e.: \"kubectl --kubeconfig=${KUBECONFIG_PATH} get nodes\" or \"export KUBECONFIG="${KUBECONFIG_PATH}"\"" || (fatal "kubectl configuration: KO")
         else
@@ -174,7 +192,7 @@ kubectl_configuration()
 }
 
 helm_rbac_config() {
-cat << EOF | kubectl apply -f -
+cat << EOF | kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f -
 apiVersion: v1
 kind: ServiceAccount
 metadata:
@@ -199,12 +217,15 @@ EOF
 helm_install()
 {
   ### install helm
-  if [ ${HELM_INSTALLED} ]; then
+  if ${HELM_INSTALLED}; then
+      info "[helm] Install"
       helm_rbac_config
       helm --kubeconfig=${KUBECONFIG_PATH} init --service-account tiller
-      info "Please wait for the tiller pod to be ready"
+      info "[helm] Please wait for the tiller pod to be ready"
+      kubectl --kubeconfig=${KUBECONFIG_PATH} rollout status deployment tiller-deploy --namespace=kube-system -w
+      [ $? -eq 0 ] && success "[helm] installation: OK" || fatal "[helm] installation: KO"
     else
-      info "helm not configured because not installed"
+      info "[helm] helm not configured because not installed"
   fi
 }
 
@@ -220,6 +241,212 @@ helm_install()
 #   helm version
 #   [ $? -eq 0 ] && success "Node ${LINE[0]} helm installation: OK" || (info "Node ${LINE[0]} helm installation: KO")
 # }
+
+metrics-server_rbac_config() {
+cat << EOF | kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: metrics-server
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: metrics-server
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: metrics-server
+    namespace: kube-system
+EOF
+}
+
+metrics-server_install()
+{
+  info "[metrics-server] Configuring ServiceAccount and ClusterRoleBinding"
+  metrics-server_rbac_config
+  info "[metrics-server] Install"
+  TMP_DIR=$(mktemp -d /tmp/k3s-XXXXXX) && cd $TMP_DIR && echo $TMP_DIR
+  git clone https://github.com/kubernetes-incubator/metrics-server.git
+  kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f metrics-server/deploy/1.8+/
+  kubectl --kubeconfig=${KUBECONFIG_PATH} rollout status deployment metrics-server --namespace kube-system -w
+  [ $? -eq 0 ] && success "[metrics-server] installation: OK" || fatal "[metrics-server] installation: KO"
+  rm -rf $TMP_DIR
+}
+
+weave-scope_install()
+{
+  [[ -n $WEAVESCOPE ]] || return 0
+  info "[weavescope] Install"
+  kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl --kubeconfig=${KUBECONFIG_PATH} version | base64 | tr -d '\n')&k8s-service-type=NodePort"
+  kubectl --kubeconfig=${KUBECONFIG_PATH} rollout status deployment weave-scope-app --namespace weave -w
+  [ $? -eq 0 ] && success "[weavescope] installation: OK" || fatal "[weavescope] installation: KO"
+  info "[weavescope] URL: http://${K3S_NODEIP_MASTER}:$(kubectl --kubeconfig=${KUBECONFIG_PATH} get services weave-scope-app --namespace weave -o jsonpath="{.spec.ports[0].nodePort}")"
+}
+
+prometheus-operator_install() {
+  [[ -n $PROMETHEUS ]] || return 0
+  info "[prometheus] Install"
+  helm --kubeconfig=${KUBECONFIG_PATH} install -n prometheus-operator --namespace monitoring stable/prometheus-operator --set grafana.service.type=NodePort --set grafana.service.nodePort=30808 --set prometheus.service.type=NodePort --set prometheus.service.nodePort=30909 --set kubelet.serviceMonitor.https=true --wait --timeout 300
+  [ $? -eq 0 ] && success "[prometheus] installation: OK" || fatal "[prometheus] installation: KO"
+  info "[prometheus] URL Grafana: http://${K3S_NODEIP_MASTER}:$(kubectl --kubeconfig=${KUBECONFIG_PATH} get services prometheus-operator-grafana --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}"), user: \"$(kubectl get secrets prometheus-operator-grafana --namespace monitoring -o jsonpath='{.data.admin-user}' | base64 --decode)\", pwd: \"$(kubectl get secrets prometheus-operator-grafana --namespace monitoring -o jsonpath='{.data.admin-password}' | base64 --decode)\""
+  info "[prometheus] URL Prometheus: http://${K3S_NODEIP_MASTER}:$(kubectl --kubeconfig=${KUBECONFIG_PATH} get services prometheus-operator-prometheus --namespace monitoring -o jsonpath="{.spec.ports[0].nodePort}")"
+}
+
+istio_grafana_gateway() {
+cat << EOF | kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: grafana-gateway
+  namespace: istio-system
+spec:
+  selector:
+    istio: ingressgateway
+  servers:
+  - port:
+      number: 15031
+      name: http-grafana
+      protocol: HTTP
+    hosts:
+    - "*"
+EOF
+}
+
+istio_grafana_virtualservice() {
+cat << EOF | kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: grafana-vs
+  namespace: istio-system
+spec:
+  hosts:
+  - "*"
+  gateways:
+  - grafana-gateway
+  http:
+  - match:
+    - port: 15031
+    route:
+    - destination:
+        host: grafana
+        port:
+          number: 3000
+EOF
+}
+
+nginx() {
+cat << EOF | kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: nginx-svc
+  labels:
+    app: nginx-svc
+spec:
+  selector:
+    app: nginx
+  ports:
+  - name: http
+    port: 80
+    nodePort: 32767
+  type: NodePort
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: nginx-deploy
+  labels:
+    version: v1
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+        version: v1
+    spec:
+      containers:
+      - name: nginx-container
+        image: nginx
+        ports:
+        - containerPort: 80
+EOF
+}
+
+istio_nginx_gateway() {
+cat << EOF | kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: nginx-gateway
+spec:
+  selector:
+    istio: ingressgateway 
+  servers:
+  - port:
+      number: 80
+      name: http
+      protocol: HTTP
+    hosts:
+    - "*"
+EOF
+}
+
+istio_nginx_virtualservice() {
+cat << EOF | kubectl --kubeconfig=${KUBECONFIG_PATH} apply -f -
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: nginx-vs
+spec:
+  hosts:
+  - "*"
+  gateways:
+  - nginx-gateway
+  http:
+  - route:
+    - destination:
+        host: nginx-svc
+EOF
+}
+
+istio_install()
+{
+  [[ -n $ISTIO ]] || return 0
+  if ${HELM_INSTALLED}; then
+      info "[istio.io] Installation ref: https://istio.io/docs/setup/kubernetes/install/helm/"
+      info "[istio.io] Adding helm repo"
+      helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.2.0/charts/
+      info "[istio.io] Installing required CRDs"
+      helm --kubeconfig=${KUBECONFIG_PATH} install --name istio-init --namespace istio-system istio.io/istio-init --wait --timeout 300
+      while [ $(kubectl --kubeconfig=${KUBECONFIG_PATH} get crds | grep 'istio.io\|certmanager.k8s.io' | wc -l | xargs 2>/dev/null) -ne 23 ]; do echo -n .; sleep 5; done; info "[istio.io] Required CRDs have been committed"
+      info "[istio.io] Install chart"
+      # --set kiali.enabled=true
+      helm --kubeconfig=${KUBECONFIG_PATH} install --name istio --namespace istio-system --set grafana.enabled=true istio.io/istio --wait --timeout 600
+      info "[istio.io] Enabling the creation of Envoy proxies for automatic sidecar injection"
+      kubectl --kubeconfig=${KUBECONFIG_PATH} label namespace default istio-injection=enabled
+      kubectl --kubeconfig=${KUBECONFIG_PATH} get namespace -L istio-injection
+      info "[istio.io] Creating Istio Objects"
+      istio_grafana_gateway
+      istio_grafana_virtualservice
+      while [ $(curl -o /dev/null -w "%{http_code}\n" -sSLIk "http://${K3S_NODEIP_WORKER}:15031" 2>/dev/null) -ne 200 ]; do echo -n .; sleep 5; done; info "[istio.io] Istio graphana: http://${K3S_NODEIP_WORKER}:15031"
+      istio_nginx_gateway
+      istio_nginx_virtualservice
+      nginx
+      while [ $(curl -o /dev/null -w "%{http_code}\n" -sSLIk "http://${K3S_NODEIP_WORKER}" 2>/dev/null) -ne 200 ]; do echo -n .; sleep 5; done; info "[istio.io] nginx: http://${K3S_NODEIP_WORKER}"
+    else
+      info "[istio.io] istio not configured because helm is not installed"
+  fi
+}
 
 clean()
 {
@@ -239,9 +466,13 @@ clean()
 
 
 # --- main script ---
+check_root
 check_prerequisite
 installation_multipass
 k3s_setup
-#k3s_labels
 kubectl_configuration
 helm_install
+metrics-server_install
+WEAVESCOPE="" weave-scope_install
+PROMETHEUS="" prometheus-operator_install
+ISTIO="y" istio_install
